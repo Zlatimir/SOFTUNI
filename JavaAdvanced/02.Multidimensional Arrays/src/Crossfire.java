f290451229d0ebea1eb7a13c35f3bcedf9733fef
ZlatimirIvanov <zlatimir_ivanov@abv.bg>
2020-12-20 20:03:34 +0200

    New file added

---
+++ JavaAdvanced/02.Multidimensional Arrays/src/Crossfire.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by Zlatimir Ivanov on 20.12.2020 г..
+ */
+
+public class Crossfire {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
