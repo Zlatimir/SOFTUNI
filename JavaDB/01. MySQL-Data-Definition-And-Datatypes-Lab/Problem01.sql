215b1716125d44775be236d0097dc8997ebda36f
ZlatimirIvanov <zlatimir_ivanov@abv.bg>
2021-04-29 14:18:40 +0300

    Some queries

--- JavaDB/01. MySQL-Data-Definition-And-Datatypes-Lab/Problem01.sql
+++ JavaDB/01. MySQL-Data-Definition-And-Datatypes-Lab/Problem01.sql
@@ -1,18 +1,17 @@
-CREATE SCHEMA `gamebar` DEFAULT CHARACTER SET utf8;
 
 CREATE TABLE `employees` (
-  `id` INT(10) NOT NULL AUTO_INCREMENT,
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
-  `last_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
+  `last_name` VARCHAR(45) NOT NULL
+  );
   
   CREATE TABLE `categories` (
-  `id` INT(10) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL
+  );
   
 CREATE TABLE `products` (
-  `id` INT(10) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `category_id` INT(10) NULL,
-  PRIMARY KEY (`id`));
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `category_id` INT NOT NULL
+);
