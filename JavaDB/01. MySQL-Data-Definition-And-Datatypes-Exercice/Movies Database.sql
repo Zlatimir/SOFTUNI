1ce1c8c74e21e6fe5924b3f4571c25bac3b43997
ZlatimirIvanov <zlatimir_ivanov@abv.bg>
2021-05-02 18:13:40 +0300

    More Queries

--- JavaDB/01. MySQL-Data-Definition-And-Datatypes-Exercice/Movies Database.sql
+++ JavaDB/01. MySQL-Data-Definition-And-Datatypes-Exercice/Movies Database.sql
@@ -41,3 +41,35 @@ ADD CONSTRAINT `fk_category`
 FOREIGN KEY (`category_id`)
 REFERENCES `categories`(`id`);
 
+INSERT INTO `directors` (`director_name`)
+VALUES 
+('Director_1'),
+('Director_2'),
+('Director_3'),
+('Director_4'),
+('Director_5');
+
+INSERT INTO `genre` (`genre_name`)
+VALUES
+('Genre_1'),
+('Genre_2'),
+('Genre_3'),
+('Genre_4'),
+('Genre_5');
+
+INSERT INTO `categories` (`category_name`)
+VALUES
+('Category_1'),
+('Category_2'),
+('Category_3'),
+('Category_4'),
+('Category_5');
+
+INSERT INTO `movies`
+(`title`,`director_id`,`copyright_year`,`length`,`genre_id`,`category_id`,`rating`)
+VALUES
+('Movie-1',3,'1998','2:45:10',5,1,10),
+('Movie-2',3,'1987','2:10:10',2,3,8),
+('Movie-3',3,'1999','1:45:10',5,4,10),
+('Movie-4',3,'2010','1:58:10',4,2,6),
+('Movie-5',3,'1998','2:45:10',1,1,10);
