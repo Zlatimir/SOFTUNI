d1912916ed5822848904275a06cb544d2b8a6b20
ZlatimirIvanov <zlatimir_ivanov@abv.bg>
2021-05-16 13:57:56 +0300

    MySQL functions

---
+++ JavaDB/07. MySQL - Database Programmability and Transactions/05. Salary Level Function.sql
@@ -0,0 +1,18 @@
+DROP FUNCTION IF EXISTS ufn_get_salary_level;
+
+DELIMITER ##
+
+CREATE FUNCTION ufn_get_salary_level(salary DECIMAL)
+RETURNS VARCHAR(20)
+DETERMINISTIC
+BEGIN
+	DECLARE salary_level VARCHAR(20);
+    SET salary_level := (
+		IF(salary<30000, 'Low',
+			IF(salary>=30000 AND salary<=50000, 'Average', 'High')));
+    RETURN salary_level;
+END##
+
+DELIMITER ;
+
+SELECT ufn_get_salary_level(100000);
