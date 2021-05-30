38ebd9a065ad1438b04742b0b5af8894d0ba615e
ZlatimirIvanov <zlatimir_ivanov@abv.bg>
2021-05-30 14:22:23 +0300

    Exam_prep - 09-02-2020

--- JavaDB/Exam_Prep_09-02-2020/07. Detail info for all teams.sql
+++ JavaDB/Exam_Prep_09-02-2020/07. Detail info for all teams.sql
@@ -1,3 +1,5 @@
 SELECT teams.`name` AS team_name, established, fan_base, COUNT(players.id) AS players_count
 FROM teams LEFT JOIN players 
-ON players.team_id = teams.id;
+ON players.team_id = teams.id
+GROUP BY teams.id
+ORDER BY players_count DESC, fan_base DESC;
