3e3b0d103185fa8979e389bf76a43f69f969875c
ZlatimirIvanov <zlatimir_ivanov@abv.bg>
2021-07-04 09:28:21 +0300

    Cleaning some mess

--- Java_SpringData/08. Exercise Introduction to Hibernate/src/main/java/Main.java
+++ Java_SpringData/08. Exercise Introduction to Hibernate/src/main/java/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
@@ -12,18 +11,4 @@ public class Main {
         em.close();
     }
 }
-=======
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
 
-public class Main {
-    public static void main(String[] args) {
-        EntityManager em = Persistence.createEntityManagerFactory("softuni").createEntityManager();
-
-        Engine engine = new Engine(em);
-        engine.run();
-
-        em.close();
-    }
-}
->>>>>>> f8966932183f5bd3236c5ff8b5dd0544494388e1
